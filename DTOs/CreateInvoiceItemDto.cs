9be47a41f17a8711be5fec6c66edb0f631a2982f
prabhakar0tenn19 <prabhakar.lal@cginfinity.com>
2026-07-27 19:52:48 +0530

    Add validation attributes to CreateInvoiceItemDto

--- DTOs/CreateInvoiceItemDto.cs
+++ DTOs/CreateInvoiceItemDto.cs
@@ -1,11 +1,14 @@
 using backend2.Models;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace backend2.DTOs{
 
     public class CreateInvoiceItemDto{
 
+        [Required(ErrorMessage = "Product ID is required")]
         public string ProductId {get; set; }=string.Empty;
+
+        [Range(1, 10000, ErrorMessage = "Quantity must be at least 1")]
         public int Quantity {get; set; }=0;
         
     }
