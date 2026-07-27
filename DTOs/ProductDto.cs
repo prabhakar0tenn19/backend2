52b6aa5e1c2460ca0d602f420c0607685603ce60
prabhakar0tenn19 <prabhakar.lal@cginfinity.com>
2026-07-27 19:54:03 +0530

    Update ProductDto.cs

--- DTOs/ProductDto.cs
+++ DTOs/ProductDto.cs
@@ -5,13 +5,17 @@ namespace backend2.DTOs{
 
     public class ProductDto{
 
-        [Required(ErrorMessage="Name is Required ")]
+        [Required(ErrorMessage="Product Name is required")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Product Name must be between 2 and 100 characters")]
+        [RegularExpression(@"^[a-zA-Z0-9\s.\-]+$", ErrorMessage = "Product Name must not contain special characters")]
         public string name{get;set;} = string.Empty;
 
-        [Range(0, 10000000, ErrorMessage="Price must be positive")]
+        [Required(ErrorMessage="Price is required")]
+        [Range(0.01, 10000000.00, ErrorMessage="Price must be greater than 0")]
         public decimal price{get;set;} = 0m;
 
-        [Range(0, 100000, ErrorMessage="Stock must be positive")]
+        [Required(ErrorMessage="Stock is required")]
+        [Range(0, 1000000, ErrorMessage="Stock must be 0 or a positive integer")]
         public int stock{get;set;} = 0;
 
         //public DateTime createdAT{get;set;} = DateTime.Now;
