f7f8e76996004b9fa2900950152bd7dc6d61226a
prabhakar0tenn19 <prabhakar.lal@cginfinity.com>
2026-07-27 19:53:11 +0530

    Add validation attributes to CreateInvoiceRequestDto

--- DTOs/CreateInvoiceRequestDto.cs
+++ DTOs/CreateInvoiceRequestDto.cs
@@ -1,9 +1,13 @@
 using backend2.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace backend2.DTOs{
     public class CreateInvoiceRequestDto{
+        [Required(ErrorMessage = "Party ID is required")]
         public string PartyId {get;set;}=string.Empty;
        
-       public List<CreateInvoiceItemDto> Items {get;set;}=new List<CreateInvoiceItemDto>();
+        [Required(ErrorMessage = "Invoice items cannot be empty")]
+        [MinLength(1, ErrorMessage = "Invoice must contain at least one item")]
+        public List<CreateInvoiceItemDto> Items {get;set;}=new List<CreateInvoiceItemDto>();
     }
 }
